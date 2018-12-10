f46f2c8845a0f68ae1e96f81824c0262e483973a
Krzysiek Piekarczyk <krzys@piekarczyk.eu>
2018-12-10 16:06:41 +0100

    add file_handling sources

---
+++ cfg_file_handling.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Krzysztof Piekarczyk on 2018-12-10.
+//
+
+#include "cfg_file_handling.h"
---
+++ cfg_file_handling.h
@@ -0,0 +1,8 @@
+//
+// Created by Krzysztof Piekarczyk on 2018-12-10.
+//
+
+#ifndef CFG_CFG_FILE_HANDLING_H
+#define CFG_CFG_FILE_HANDLING_H
+
+#endif //CFG_CFG_FILE_HANDLING_H
